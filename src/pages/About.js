c8f1d7f590fb0786f20ca1139201a00abece526f
Leandro Vitto <leandro.vitto@webscience.it>
2022-04-29 00:31:42 +0200

    navigate

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,26 @@
 import React from 'react'
-import Container from "../components/layout/Container";
-import Sidebar from "../components/layout/Sidebar";
+import { useNavigate } from 'react-router';
+import { UiButton } from '../components/ui/UiButton';
 
 const About = () => {
+  let navigate = useNavigate();
+
+  const redirectToHome = () => {
+    /* setTimeout(() => {
+      navigate("/")
+    },2000) */
+    navigate("/")
+  }
+
   return (
     <div>
-      <div className="border-dashed border-2 border-gray-800 h-48 p-6 text-2xl rounded">About</div>
+      <div className="border-dashed border-2 border-gray-800 h-48 p-6 text-2xl rounded">
+        About
+        <UiButton
+          title="Go To Home"
+          onClick={redirectToHome}
+        />
+      </div>
     </div>
   )
 }
