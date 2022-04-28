b7aa187cf87c05ef7394d98b099361285f1ff42a
Leandro Vitto <leandro.vitto@webscience.it>
2022-04-29 00:27:12 +0200

    pathaname

--- src/components/layout/Container.js
+++ src/components/layout/Container.js
@@ -1,12 +1,16 @@
 import React from 'react'
-import { Outlet } from 'react-router'
+import { Outlet, useLocation } from 'react-router'
 
 const Container = ({children}) => {
+  let location = useLocation();
   return (
     <div className="md:pl-64 flex flex-col flex-1">
         <main className="flex-1">
             <div className="py-6">
                 <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
+                  <div className="text-base font-bold border-b-2 border-gray-200 mb-4 capitalize">
+                    {location.pathname}
+                  </div>
                   <Outlet />
                   {children}
                 </div>
