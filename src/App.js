6f91a0bdd1e990d85bf4a698304c233204251d10
Leandro Vitto <leandro.vitto@webscience.it>
2022-04-28 23:55:52 +0200

    React Router Init

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+import { Route, Routes } from "react-router";
 import About from "./pages/About";
 import Home from "./pages/Home";
 
 export default function App() {
   return (
     <div>
-       <Home />
-       <About />
+       <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Routes>
     </div>
   );
 }
