f622b6205140237cdaab315cd97973ed02150ec4
Isjeady <admin@isjeady.com>
2022-05-06 15:37:40 +0200

    Card Products

--- src/components/ProductsList.js
+++ src/components/ProductsList.js
@@ -1,20 +1,26 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import ProductItem from './ProductItem';
 
 const products = [
     {id : 1, name : "Prodotto Uno", slug : "prodotto-uno"},
     {id : 2, name : "Prodotto Due", slug : "prodotto-due"},
     {id : 3, name : "Prodotto Tre", slug : "prodotto-tre"},
+    {id : 4, name : "Prodotto Tre", slug : "prodotto-tre"},
+    {id : 5, name : "Prodotto Tre", slug : "prodotto-tre"},
+    {id : 6, name : "Prodotto Tre", slug : "prodotto-tre"},
+    {id : 7, name : "Prodotto Tre", slug : "prodotto-tre"},
+    {id : 8, name : "Prodotto Tre", slug : "prodotto-tre"},
 ];
 
 
 const ProductsList = () => {
   return (
     <div>
-        <ul className="list-disc">
+         <ul role="list" className="grid grid-cols-4 gap-6">
             {products.map(product => {
-                return <li>
-                    <Link to={`/products/${product.slug}`}>{product.name}</Link>
+                return <li key={product.id}>
+                    <ProductItem product={product} />
                 </li>
             })}
         </ul>
