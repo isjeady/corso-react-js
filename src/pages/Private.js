9f8cf44e4ccb5374decb2b5e13051fb9eb506688
Isjeady <admin@isjeady.com>
2022-06-06 23:30:48 +0200

    Fix Logout

--- src/pages/Private.js
+++ src/pages/Private.js
@@ -2,13 +2,14 @@ import React from 'react'
 import { useNavigate } from 'react-router-dom';
 import RequireAuth from '../components/layout/RequireAuth'
 import { UiButton } from '../components/ui/UiButton'
+import { LOCAL_STORAGE_TOKEN_PROPS } from '../services/http';
 
 const Private = () => {
   let navigate = useNavigate();
 
   const handleLogout = () => {
     if(confirm("Vuoi uscire??")){
-        localStorage.removeItem("auth");
+        localStorage.removeItem(LOCAL_STORAGE_TOKEN_PROPS);
         navigate("/login")
     }
   }
