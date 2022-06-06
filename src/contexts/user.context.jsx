d987ea7ceec354121da2e49be97d36fd04780b61
Isjeady <admin@isjeady.com>
2022-06-07 00:15:25 +0200

    Reducer Init Configuration

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -5,6 +5,28 @@ export const UserContext = createContext({
     setCurrentUser : () => null
 });
 
+export const USER_ACTION_TYPES = {
+    SET_CURRENT_USER: 'SET_CURRENT_USER',
+};
+  
+const INITIAL_STATE = {
+    currentUser: null,
+};
+
+const userReducer = (state, action) => {
+    const { type, payload } = action;
+  
+    switch (type) {
+      case USER_ACTION_TYPES.SET_CURRENT_USER:
+        return { 
+            ...state, 
+            currentUser: payload 
+        };
+      default:
+        throw new Error(`Error Type:${type} in userReducer`);
+    }
+};
+
 export const UserProvider = ({ children }) => {
     const [currentUser,setCurrentUser] = useState(null);
 
