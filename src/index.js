b6751d82e45bdc4b5c66af87c4810a57459f12d4
Isjeady <admin@isjeady.com>
2022-09-08 17:27:40 +0200

    3 - Configure Store

--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
 import { UserProvider } from "./contexts/user.context";
+import { store } from "./store/store";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <StrictMode>
-    <BrowserRouter>
-      <UserProvider>
-        <App />
-      </UserProvider>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <UserProvider>
+          <App />
+        </UserProvider>
+      </BrowserRouter>
+    </Provider>
   </StrictMode>,
   rootElement
 );
--- src/store/store.js
+++ src/store/store.js
@@ -1,2 +1,12 @@
-import { compose, configureStore, applyMiddleware } from 'redux';
+
+import { compose,configureStore } from '@reduxjs/toolkit';
+import { applyMiddleware } from 'redux';
 import logger from 'redux-logger'
+
+import { rootReducer } from './root.reducer';
+
+const middleWares = [logger];
+
+const composedEnhancers = compose(applyMiddleware(...middleWares));
+
+export const store = configureStore({ reducer: rootReducer, enhancers: composedEnhancers})
